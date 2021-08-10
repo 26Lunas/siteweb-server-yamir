a75ca68391c75b613216dc806893cf69a4b84d14
26Lunas <88166789+26Lunas@users.noreply.github.com>
2021-08-10 14:50:58 -0500

    Update respon-menu.js

--- respon-menu.js
+++ respon-menu.js
@@ -1,9 +1,24 @@
+
 addEventListener('DOMContentLoaded', () => {
     const boton = document.querySelector('.btn-menu');
     if (boton){
         boton.addEventListener('click', () => {
             const menu = document.querySelector('.menu');
-            menu.classList.toggle('desplecable')
+            menu.classList.toggle('desplecable');
         })
     }
+    const quitarMenu = document.querySelectorAll('.menu a');
+    console.log(quitarMenu)
+    if (quitarMenu){
+        for (let i=1; i<7; i++) {
+            quitarMenu[i].addEventListener('click', () => {
+                const menu = document.querySelector('.menu');
+                menu.classList.toggle('desplecable');
+            }) 
+        }
+    }
+    addEventListener('scroll', () => {
+        const menu = document.querySelector('.menu');
+            menu.classList.remove('desplecable');
+    })
 }) 
